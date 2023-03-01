d6abde596e0725f1d3752c36f326c478536b003c
Ricardo Cambui <r.cambui@hotmail.com>
2023-03-01 14:26:53 -0300

    Desafio adicionado

---
+++ src/algorithms/Staircase.java
@@ -0,0 +1,72 @@
+package algorithms;
+
+/*Staircase detail
+
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of . */
+
+public class Staircase {
+    
+    public static void staircase(int n) {
+        String stairs = "#";
+        int m = n-1;
+        for (int i=1; i<=n; i++) {
+            for (int j=m; j>=1; j--) {
+                System.out.print(" ");
+            }
+            System.out.print(stairs);
+            if (i == n) { break;}
+            stairs += "#";
+            System.out.println();
+            m--;
+        }
+
+    }
+}
